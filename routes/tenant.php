4bf77199352b692bca7b0802c86ccecfd93777cd
crazyboy611 <129376844+crazyboy611@users.noreply.github.com>
2026-04-06 14:15:34 +0700

    feat(admin auth): admin authentication and ui

--- routes/tenant.php
+++ routes/tenant.php
@@ -23,7 +23,7 @@ Route::middleware([
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-    Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
-    });
+    // Route::get('/', function () {
+    //     return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+    // });
 });
