6b2dba6e81055327f60a9f9a9d457e0aa1a75e8a
crazyboy611 <129376844+crazyboy611@users.noreply.github.com>
2026-04-29 20:46:03 +0700

    refactor(plan): remove price_yearly

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -15,21 +15,18 @@ class PlanSeeder extends Seeder
                 'max_fields' => 3,
                 'max_staff' => 2,
                 'price_monthly' => 200000,
-                'price_yearly' => 2000000,
             ],
             [
                 'name' => 'Nang cao',
                 'max_fields' => 10,
                 'max_staff' => 5,
                 'price_monthly' => 500000,
-                'price_yearly' => 5000000,
             ],
             [
                 'name' => 'Premium',
                 'max_fields' => 50,
                 'max_staff' => 20,
                 'price_monthly' => 1000000,
-                'price_yearly' => 10000000,
             ],
         ];
 
