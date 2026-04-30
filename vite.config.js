b0e688297a6a0ac4fbf3a054facc75ec21341185
crazyboy611 <129376844+crazyboy611@users.noreply.github.com>
2026-04-30 16:06:33 +0700

    refactor(vite.config.js)

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    server: {
-        cors: true,
-        hmr: {
-            host: 'localhost',
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/js/app.tsx', 'resources/css/app.css'],
